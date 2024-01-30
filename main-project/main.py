49a6941379f42199ddc1c7acf6c8f7a194d40f08
Jay Shin <shin.810@osu.edu>
2024-01-30 18:26:49 -0500

    Set up Flask and a template for passing variables from Python to  HTML

--- main-project/main.py
+++ main-project/main.py
@@ -1,3 +1,24 @@
 from elasticsearch import Elasticsearch
+from flask import Flask, render_template
 
 print("hello world!")
+
+app = Flask(__name__)
+
+def function_to_pass():
+    return "Hello world"
+
+def function_to_pass_2():
+    return "Buy this stock because blah blah"
+
+@app.route('/')
+def index():
+    flaskText1 = function_to_pass()
+    flaskText2 = function_to_pass_2()
+    
+    # passing 2 strings to HTML
+    return render_template('index.html', flaskText1 = flaskText1, flaskText2 = flaskText2) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
---
+++ main-project/static/script.js
@@ -0,0 +1,8 @@
+function replaceButtonWithText() {
+    var contentContainer = document.querySelector('.content');
+    var newTextElement = document.createElement('p');
+    newTextElement.textContent = flaskText2; // passed from HTML
+    var buttonElement = document.querySelector('button');
+    buttonElement.remove();
+    contentContainer.appendChild(newTextElement);
+}
