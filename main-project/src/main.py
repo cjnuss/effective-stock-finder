8b0a6fad2fdbcbe7277a578006509f4b53fd83ab
Jay Shin <shin.810@osu.edu>
2024-02-08 17:10:09 -0500

    created a Stock class in Python to pass test stock objects to html

--- main-project/src/main.py
+++ main-project/src/main.py
@@ -2,9 +2,31 @@ from elasticsearch import Elasticsearch
 from flask import Flask, render_template
 import random
 
+class Stock:
+    def __init__(self,name,short_name,price,percentage_change,price_change):
+        self.name = name
+        self.short_name = short_name
+        self.price = price
+        self.percentage_change = percentage_change
+        self.price_change = price_change
+    
+    def create_stocks_test():
+        apple = Stock("Apple, Inc.", "AAPL", "100", "11", "11")
+        microsoft = Stock("Microsoft", "MSFT", "100", "11", "11")
+        google = Stock("Google", "GOOG", "100", "11", "11")
+        amazon = Stock("Amazon", "AMZN", "100", "11", "11")
+        tesla = Stock("Tesla", "TLSA", "100", "11", "11")
+        facebook = Stock("Facebook", "FB", "100", "11", "11")
+        nvidia = Stock("NVIDIA", "NNVDA", "100", "11", "11")
+        amd = Stock("AMD", "AMD", "100", "11", "11")
+        netflix = Stock("Netflix", "NFLX", "100", "11", "11")
+        intc = Stock("INTC", "INTC", "100", "11", "11")
+        paypal = Stock("PayPal", "PYPL", "100", "11", "11")
+
+        return [apple, microsoft, google, amazon, tesla, facebook, nvidia, amd, netflix, intc, paypal]
 
 def GetDataFromGPT():
-    stock_names = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "FB", "NVDA", "AMD", "NFLX", "INTC", "PYPL", "ADBE"]
+    stock_names = Stock.create_stocks_test()
     
     # Scramble the array
     random.shuffle(stock_names)
