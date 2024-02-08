b97ce611f39c7df2a4e12e9717d9079ebaa19b62
DrBuckWho <Payneclark360@gmail.com>
2024-02-08 16:53:56 -0500

    GPT DATA SIMULATED
    Simulated getting data from CHATGPT

--- main-project/src/main.py
+++ main-project/src/main.py
@@ -1,24 +1,30 @@
 from elasticsearch import Elasticsearch
 from flask import Flask, render_template
+import random
 
-print("hello world!")
+
+def GetDataFromGPT():
+    stock_names = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "FB", "NVDA", "AMD", "NFLX", "INTC", "PYPL", "ADBE"]
+    
+    # Scramble the array
+    random.shuffle(stock_names)
+    
+    # Select a random number of elements from 1 to 10
+    num_elements = random.randint(1, min(len(stock_names), 10))
+    
+    # Take the first num_elements from the scrambled array
+    selected_stock_names = stock_names[:num_elements]
+    return selected_stock_names
 
 app = Flask(__name__)
 
-def function_to_pass():
-    return "Hello world"
 
-def function_to_pass_2():
-    return "Buy this stock because blah blah"
 
 @app.route('/')
 def index():
-    flaskText1 = function_to_pass()
-    flaskText2 = function_to_pass_2()
+
     
-    # passing 2 strings to HTML
-    return render_template('index.html', flaskText1 = flaskText1, flaskText2 = flaskText2) 
+    return render_template('index.html', variables=GetDataFromGPT())
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
